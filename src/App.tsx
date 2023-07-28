58b06ca675eced87b07b7426f4eb8b98856ab75c
Julius Kirsch <Julius@skirsch.de>
2023-07-28 09:41:48 +0200

    feat: updated ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,33 @@
-import React from 'react';
-import './styles/app.css';
-import { Interpreter } from './juli/Interpreter';
+import React from "react";
+import "./styles/app.css";
+import { Interpreter } from "./juli/Interpreter";
+import { OutputHandler } from "./juli/OutputHandler";
 
-function runCode(){
-  const input = document.getElementById("codeinput") as HTMLTextAreaElement;
-  console.log("CODE: " + input.value);
-  const result = new Interpreter(input.value).interpret();
+let outputHandler: OutputHandler;
 
-  const output = document.getElementById("output") as HTMLElement;
+function runCode() {
+  if(outputHandler === undefined){
+    outputHandler = new OutputHandler(document.getElementById("output") as HTMLElement);
+  }
+  else {
+    outputHandler.output.innerText = "";
+  }
+    const input = document.getElementById("codeinput") as HTMLTextAreaElement;
+    new Interpreter(input.value, outputHandler).interpret();
 }
 
 function App() {
-  return (
-    <div className='app'>
-<textarea className='textInput' id='codeinput' ></textarea>
-<div className='runbtn' onClick={runCode}>Run</div>
-<div className='output' id='output'></div>
-    </div>
-  );
+    return (
+        <div className="app">
+          <div className="navigationBar">
+            <div className="navigationBarItem runbtn"  onClick={runCode}>Run Code</div>
+          </div>
+          <div className="content">
+            <textarea className="codeInput" id="codeinput"/>
+            <div className="output" id="output"></div>
+          </div>
+        </div>
+    );
 }
 
 export default App;
